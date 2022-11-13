a7a25d63dc4969a9cfcabb809e4a5cb6ee523129
Alexander <caminante.msk@gmail.com>
2022-11-12 23:13:17 -0300

    header + footer, layouts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,16 +14,19 @@ const Footer = () => {
         <div className="footer__row">
           <a href="https://wa.me/5521967261434" className="footer__item">
             <img src={WhatsappIcon} alt="whatsapp" />
+            <p>21-99936-5384</p>
           </a>
           <div className="footer__item">
             <img src={EmailIcon} alt="Email" />
+            <p>contato@eformaliza.com</p>
           </div>
           <a href="https://www.instagram.com/eformaliza/" className="footer__item">
             <img src={InstagramIcon} alt="Instagram" />
+            <p>@eformaliza</p>
           </a>
         </div>
         <div className="footer__box">
-          <div className="footer__unit">
+          <div className="footer__dove">
             <img src={DoveImg} alt="Peace" />
           </div>
           <div className="footer__unit">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
 import './Header.scss'
+import logo from "./../../images/icons/logo_principal.svg"
+import enterIcon from "./../../images/icons/enter.svg"
+import { useState } from "react"
 
 const Header = () => {
+
+  const [auth, setAuth] = useState(false)
+
   return (
-    <header>
-      Header 
+    <header className="header">
+      <div className="container">
+        <div className="header__row">
+          <a classname="header__logo" href="./">
+            <img src={logo} alt="eformaliza" className="logo"/>
+          </a>
+          <div className="header__title">
+            <h1>Blog do seu negócio</h1>
+          </div>
+          <div className="header__auth">
+            {!auth 
+            ? <button className="btn" title="Entrar">
+              <img src={enterIcon} alt="entrar" />
+            </button>
+            : <>
+                <p>Usuario</p>
+                <button className="btn">Sair</button>
+              </>}
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
